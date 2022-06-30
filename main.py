0f39db89a421c06670895315b48110cac36f261f
Huiqi Luo <lhq7563@163.com>
2022-06-30 17:03:19 +0100

    run each model and save forecast vallue in json file

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
+from posixpath import split
 import random
 import time
-from math import sqrt
 
-import matplotlib.pyplot as plt
 import numpy as np
-import orbit
 import pandas as pd
-import pmdarima as pm
 import tqdm
-import xgboost as xgb
 import os
 import json
-from arch import arch_model
-from orbit.diagnostics.plot import plot_predicted_data
-from orbit.models import ETS
 from orbit.utils.dataset import load_m3monthly
-from pandas import Series
 from pandas_datareader import data
-from PyEMD import CEEMDAN, EMD, Visualisation
-from pyentrp import entropy as ent
-from sklearn.metrics import (make_scorer, mean_absolute_error,
-                             mean_squared_error)
-from sklearn.model_selection import GridSearchCV, KFold, train_test_split
-from sklearn.preprocessing import StandardScaler
-from statsmodels.stats.diagnostic import acorr_ljungbox, het_arch
-from statsmodels.tsa.arima.model import ARIMA
-from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
 
 from Wrapper import ARIMAWrapper,CEEMDAN_PE_ARIMAWrapper,XGBoostWrapper,CEEMDAN_PE_XGBoostWrapper
 
@@ -39,9 +23,11 @@ def confirm_json():
     target_path = f'./Target-Transformation/prediction_results/result_{int(time.time())}.json'
     print(target_path)
     os.rename('./results.json.temp',target_path)
+
 def init_json():
     if os.path.exists('./results.json.temp'):
         os.remove('./results.json.temp')
+
 def sample_keys(n_sample=1):
     ### M3 Dataset
     data = load_m3monthly()
@@ -57,7 +43,13 @@ def sample_keys(n_sample=1):
 if __name__ == '__main__':
     init_json()
     
+    key_col='key'
+    response_col='value'
+    date_col='date'
+    
+
     for key in tqdm.tqdm(sample_keys()):
+        split = 10
         # Initial new dict
         info_dict = {
             'dataset_name' : key,
@@ -67,39 +59,45 @@ if __name__ == '__main__':
             'xgboost': dict(),
             'xgboost_transf': dict()
         }
-
-        
-        # Set col according to the original dataset
-        data = load_m3monthly()
-        key_col='key'
-        response_col='value'
-        date_col='date'
         # Data precessing
+        data = load_m3monthly()
         df = data[data[key_col] == key]
         df_copy = df.copy()
         infer_freq = pd.infer_freq(df_copy[date_col])
         df_copy = df_copy.set_index(date_col)
         df_copy = df_copy.asfreq(infer_freq).drop(['key'], axis=1)
         df_copy = df_copy['value']
-
+        # save test data
+        train_y,test_y = train_test_split(df_copy,test_size=split,shuffle=False)
+        test_y = test_y.values.tolist()
+        train_y = train_y.values.tolist()
+ 
         # Fit model
         arima = ARIMAWrapper(df_copy).fit_predict()
-        # arima_transf = CEEMDAN_PE_ARIMAWrapper(df_copy).fit_predict()
-        # xgboost = XGBoostWrapper(df_copy).fit_predict()
-        # xgboost_transf = CEEMDAN_PE_XGBoostWrapper(df_copy).fit_predict()
+        arima_transf = CEEMDAN_PE_ARIMAWrapper(df_copy).fit_predict()
+        xgboost = XGBoostWrapper(df_copy).fit_predict()
+        xgboost_transf = CEEMDAN_PE_XGBoostWrapper(df_copy).fit_predict()
 
         # Save forecast value into info_dict 
         info_dict['arima'] = {
-            'pred' : arima
-        # }
-        # info_dict['arima_tranf'] = {
-        #     'pred' : arima_transf
-        # }
-        # info_dict['xgboost'] = {
-        #     'pred' : xgboost
-        # }
-        # info_dict['xgboost_transf'] = {
-        #     'pred' : xgboost_transf
+            'pred' : arima,
+            'test': test_y,
+            'train': train_y
+        }
+        info_dict['arima_tranf'] = {
+            'pred' : arima_transf,
+            'test': test_y,
+            'train': train_y
+        }
+        info_dict['xgboost'] = {
+            'pred' : xgboost,
+            'test': test_y,
+            'train': train_y
+        }
+        info_dict['xgboost_transf'] = {
+            'pred' : xgboost_transf,
+            'test': test_y,
+            'train': train_y 
         }
 
         save_json(info_dict)
